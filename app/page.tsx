3c57846be262f749456ef2647dfbddf7a70fa4a6
paresiqbal <thecrowdel@gmail.com>
2023-11-01 22:12:46 +0700

    All setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import Image from "next/image";
 
+// components
+import Nav from "@/components/Nav";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main className="p-8">
+      <Nav />
+      <section className="py-24 flex flex-col gap-8 items-center text-center">
+        <h1 className="text-4xl font-bold">Chadcn is awesome</h1>
+        <p className="text-2xl text-muted-foreground">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In,
+          necessitatibus.
+        </p>
+      </section>
+      <div className="flex justify-center">
+        <Button>Click Me 😉</Button>
+        <Button>Learn Now</Button>
+      </div>
     </main>
   );
 }
---
+++ components/Nav.tsx
@@ -0,0 +1,7 @@
+export default function nav() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">Next 14 + Chadcn</h1>
+    </div>
+  );
+}
